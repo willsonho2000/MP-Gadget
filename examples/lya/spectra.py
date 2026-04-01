77c30afc8557405f6cdc9f78e14be0b4ee859554
myho <mho101@ucr.edu>
2026-03-31 22:50:35 -0700

    Lym alpha plot

--- examples/lya/spectra.py
+++ examples/lya/spectra.py
@@ -4,10 +4,12 @@ from fake_spectra.griddedspectra import GriddedSpectra
 from mpi4py import MPI
 
 # rr = RandSpectra(5, "MySim", MPI=MPI, thresh=0.)
-gs = GriddedSpectra(1, "output_local", nspec=10, MPI=MPI)
-gs.get_tau("H",1,1215)
-#Lyman-beta
-gs.get_tau("H",1,1025)
-gs.get_col_density("H",1)
-#Save spectra to file
-gs.save_file()
+for i in range(14):
+
+    gs = GriddedSpectra(i, "output", nspec=10, MPI=MPI, nbins=1000)
+    gs.get_tau("H",1,1215)
+    #Lyman-beta
+    gs.get_tau("H",1,1025)
+    gs.get_col_density("H",1)
+    #Save spectra to file
+    gs.save_file()
